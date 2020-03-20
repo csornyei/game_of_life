0f84454057abdce58a7fc9c666ab48998411b4d0
Mate Csornyei <mate@csornyei.com>
2020-03-20 21:44:34 +0100

    Reformat

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,31 +1,3 @@
-function onWidthChange(event) {
-    const newWidth = event.target.value
-    updateCELLS(gameAreaSize.width, gameAreaSize.height, newWidth, gameAreaSize.height);
-    gameAreaSize.width = newWidth;
-    widthNumberInput.value = gameAreaSize.width;
-    widthRange.value = gameAreaSize.width;
-    changeGameArea();
-}
-
-function onHeightChange(event) {
-    const newHeight = event.target.value;
-    updateCELLS(gameAreaSize.width, gameAreaSize.height, gameAreaSize.width, newHeight);
-    gameAreaSize.height = newHeight;
-    heightNumberInput.value = gameAreaSize.height;
-    heightRange.value = gameAreaSize.height;
-    changeGameArea();
-}
-
-function onSpeedChange(event) {
-    const newSpeed = event.target.value;
-    speedRange.value = newSpeed;
-    speedNumberInput.value = newSpeed;
-    clearInterval(interval);
-    interval = setInterval(() => {
-        reDraw();
-    }, newSpeed);
-}
-
 function changeGameArea() {
     canvas.width = gameAreaSize.width * GRID_PIXEL_SIZE;
     canvas.height = gameAreaSize.height * GRID_PIXEL_SIZE;
@@ -204,19 +176,5 @@ function liveOrDie(cellValues) {
                 }
             }
         }
-    }
-}
-
-function stepButtonClickHandler() {
-    step = true;
-    game = true;
-}
-
-function startButtonClickHandler() {
-    game = !game;
-    if (game) {
-        startStopButton.textContent = "Stop Game";
-    } else {
-        startStopButton.textContent = "Start Game";
     }
 }
--- scripts/globals.js
+++ scripts/globals.js
@@ -2,8 +2,8 @@ const gameAreaSize = {
     "width": 25,
     "height": 25,
 };
-const frameLength = 100;
 const GRID_PIXEL_SIZE = 15;
+const frameLength = 100;
 const CELLS = [];
 let game = false;
 let step = false;
